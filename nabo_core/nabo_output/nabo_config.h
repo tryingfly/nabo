abe574a501416d418a1cd225e5dcfe4ad78422ab
tryingfly <yin123yyp@qq.com>
2024-03-26 13:12:26 +0800

    vs2017/2019 verified

--- nabo_core/nabo_output/nabo_config.h
+++ nabo_core/nabo_output/nabo_config.h
@@ -1,6 +1,7 @@
 /*=========== ***doc description @ yyp*** ===========
-This is part of Nabo (Naughty Boy, 小炮), an open project for the
-control of biped robot，Copyright (C) 2024 YYP, Shanghai Jiao Tong University, under MIT License.
+This is part of Nabo (Naughty Boy, 小炮), an open project for the control of biped robot，
+Copyright (C) 2024 YYP, Shanghai Jiao Tong University, under MIT License.
+Feel free to use in any purpose, and cite Nabo or 小炮 in any style, to contribut to the advancement of the community.
 <https://github.com/tryingfly/nabo>
 <https://www.zhihu.com/column/c_1746480685987373057>
 
